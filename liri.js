15a4a0de05542199f8a0cec0a00debce22132395
ceckenrode <chriseckenrode@me.com>
2016-01-13 15:41:17 -0500

    added twitter acess keys, experimenting with grabbing api data

---
+++ liri.js
@@ -0,0 +1,20 @@
+var twitter = require("./keys.js");
+var twitterKeys = twitter.twitterKeys;
+var command = process.argv[2].toLowerCase();
+
+switch(command){
+  case 'my-tweets':
+
+  break;
+  case 'spotify-this-song':
+
+  break;
+  case 'movie-this':
+
+  break;
+  case 'do-what-it-says':
+
+  break;
+  default: console.log("Error. Invalid argument.");
+  break;
+}
